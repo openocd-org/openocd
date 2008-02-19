6057fc8ab8a0e4395b1675250d03e199976186eb
mifi <mifi@b42882b7-edfa-0310-969c-e2dbd0fdcd60>
2008-02-19 21:44:19 +0000

    - added patch which fixed big/small endian problems on non-x86 host CPUs. https://lists.berlios.de/pipermail/openocd-development/2008-February/000846.html (thanks to Øyvind for the patch)
    git-svn-id: svn://svn.berlios.de/openocd/trunk@313 b42882b7-edfa-0310-969c-e2dbd0fdcd60

--- src/helper/types.h
+++ src/helper/types.h
@@ -40,53 +40,40 @@ typedef unsigned int u32;
 typedef unsigned long long u64;
 #endif
 
-#ifdef WORDS_BIGENDIAN /* big endian host */
 
-#define le_to_h_u32(x) (u32)((x)[0] | (x)[1] << 8 | (x)[2] << 16 | (x)[3] << 24)
-#define le_to_h_u16(x) (u16)((x)[0] | (x)[1] << 8)
-#define be_to_h_u32(x) (*(u32*)(x))
-#define be_to_h_u16(x) (*(u16*)(x))
 
-#define h_u32_to_le(buf, val) \
-	do { \
-		(buf)[3] = ((val) & 0xff000000) >> 24; \
-		(buf)[2] = ((val) & 0x00ff0000) >> 16; \
-		(buf)[1] = ((val) & 0x0000ff00) >> 8; \
-		(buf)[0] = ((val) & 0x000000ff); \
-	} while (0)
+/* DANGER!!!! here be dragons! Note that the pointer in 
+ * memory might be unaligned. On some CPU's, i.e. ARM7,
+ * the 2 lsb are ignored for 32 bit access, on others
+ * it will cause an exception and on e.g. x86, it works
+ * the same as if aligned.
+ */
+#define le_to_h_u32(x) ((u32)((x)[0] | (x)[1] << 8 | (x)[2] << 16 | (x)[3] << 24))
+#define le_to_h_u16(x) ((u16)((x)[0] | (x)[1] << 8))
+#define be_to_h_u32(x) ((u32)((x)[3] | (x)[2] << 8 | (x)[1] << 16 | (x)[0] << 24))
+#define be_to_h_u16(x) ((u16)((x)[1] | (x)[0] << 8))
 
-#define h_u16_to_le(buf, val)  \
-	do { \
-		(buf)[1] = ((val) & 0xff00) >> 8; \
-		(buf)[0] = ((val) & 0x00ff); \
-	} while (0)
+#define h_u32_to_le(buf, val) do {\
+(buf)[3] = ((val) & 0xff000000) >> 24;\
+(buf)[2] = ((val) & 0x00ff0000) >> 16;\
+(buf)[1] = ((val) & 0x0000ff00) >> 8;\
+(buf)[0] = ((val) & 0x000000ff);\
+} while (0)
+#define h_u32_to_be(buf, val) do {\
+(buf)[0] = ((val) & 0xff000000) >> 24;\
+(buf)[1] = ((val) & 0x00ff0000) >> 16;\
+(buf)[2] = ((val) & 0x0000ff00) >> 8;\
+(buf)[3] = ((val) & 0x000000ff);\
+} while (0)
 
-#define h_u32_to_be(buf, val) do { *(u32*)(buf) = (val); } while (0)
-#define h_u16_to_be(buf, val) do { *(u16*)(buf) = (val); } while (0)
+#define h_u16_to_le(buf, val) do {\
+(buf)[1] = ((val) & 0xff00) >> 8;\
+(buf)[0] = ((val) & 0x00ff) >> 0;\
+} while (0)
+#define h_u16_to_be(buf, val) do {\
+(buf)[0] = ((val) & 0xff00) >> 8;\
+(buf)[1] = ((val) & 0x00ff) >> 0;\
+} while (0)
 
-#else /* little endian host */
-
-#define le_to_h_u32(x) (*(u32*)(x))
-#define le_to_h_u16(x) (*(u16*)(x))
-#define be_to_h_u32(x) (u32)((x)[3] | (x)[2] << 8 | (x)[1] << 16 | (x)[0] << 24)
-#define be_to_h_u16(x) (u16)((x)[1] | (x)[0] << 8)
-
-#define h_u32_to_le(buf, val) do { *(u32*)(buf) = (val); } while (0)
-#define h_u16_to_le(buf, val) do { *(u16*)(buf) = (val); } while (0)
-
-#define h_u32_to_be(buf, val) \
-	do { \
-		(buf)[0] = ((val) & 0xff000000) >> 24; \
-		(buf)[1] = ((val) & 0x00ff0000) >> 16; \
-		(buf)[2] = ((val) & 0x0000ff00) >> 8; \
-		(buf)[3] = ((val) & 0x000000ff); \
-	} while (0)
-
-#define h_u16_to_be(buf, val) \
-	do { \
-		(buf)[0] = ((val) & 0xff00) >> 8; \
-		(buf)[1] = ((val) & 0x00ff); \
-	} while (0)
-#endif
 
 #endif /* TYPES_H */
