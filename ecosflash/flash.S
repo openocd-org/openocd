2e21083c19eb6f0336fb4986215ca390f422a8cc
oharboe <oharboe@b42882b7-edfa-0310-969c-e2dbd0fdcd60>
2008-03-02 08:29:44 +0000

    fixed FSF address. Uwe Hermann.
    git-svn-id: svn://svn.berlios.de/openocd/trunk@430 b42882b7-edfa-0310-969c-e2dbd0fdcd60

--- ecosflash/flash.S
+++ ecosflash/flash.S
@@ -15,7 +15,7 @@
 ##
 ## You should have received a copy of the GNU General Public License along
 ## with eCos; if not, write to the Free Software Foundation, Inc.,
-## 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+## 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 ##
 ## As a special exception, if other files instantiate templates or use macros
 ## or inline functions from this file, or you compile this file and link it
