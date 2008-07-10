313547ddd66b3dc7a8f1c6261621d26939d9d524
oharboe <oharboe@b42882b7-edfa-0310-969c-e2dbd0fdcd60>
2008-07-10 06:44:11 +0000

    very slight cleanup of flash banks handling.
    git-svn-id: svn://svn.berlios.de/openocd/trunk@782 b42882b7-edfa-0310-969c-e2dbd0fdcd60

--- src/tcl/commands.tcl
+++ src/tcl/commands.tcl
@@ -34,25 +34,22 @@ proc exit {} {
 	openocd_throw exit
 }
 
+# We have currently converted only "flash banks" to tcl.
+proc flash args {
+	if {[string compare [lindex $args 0] banks]==0} {
+		return [flash_banks_pretty]
+	}
+	openocd_throw "flash $args"
+}
+
 # If a fn is unknown to Tcl, we try to execute it as an OpenOCD command
 proc unknown {args} {
-
-	# This is uglier than it needs to be since the "flash banks" is really
-	# a single command. For now only "flash banks" has been converted from
-	# C to Tcl as an example, but if we do decide to go down this path, then
-	# some more generic scheme will be put in place here.
-	#
-	# Help texts need a makeover. There needs to be help texts for
-	# tcl procs + perhaps some work w.r.t. making the help command
-	# format things prettier.
-	if {[string compare [lindex $args 0] flash]==0 && [string compare [lindex $args 1] banks]==0} {
-		return [flash_banks_pretty]
-	}  
-
-	# We print out as we run the command
 	if {[string length $args]>0} {
-		openocd_throw "$args"
+		openocd_throw $args
 	}
+	# openocd_throw outputs while running and also sets the
+	# primary return value to the output of the command
+	#
 	# The primary return value have been set by "openocd" above,
 	# so we need to clear it, lest we print out the output from
 	# the command twice.
