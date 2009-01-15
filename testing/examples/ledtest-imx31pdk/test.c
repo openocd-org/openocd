cb3f970b6c61d0643839f9261ddcce2b8664eac7
ntfreak <ntfreak@b42882b7-edfa-0310-969c-e2dbd0fdcd60>
2009-01-15 20:58:17 +0000

    - add missing svn props from 1323 commit
    git-svn-id: svn://svn.berlios.de/openocd/trunk@1324 b42882b7-edfa-0310-969c-e2dbd0fdcd60

--- testing/examples/ledtest-imx31pdk/test.c
+++ testing/examples/ledtest-imx31pdk/test.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009 by Alan Carvalho de Assis       		   *
+ *   Copyright (C) 2009 by Alan Carvalho de Assis       		           *
  *   acassis@gmail.com                                                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
