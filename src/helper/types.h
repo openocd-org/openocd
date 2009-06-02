cd5e09303c2031b4fb91a38a7ebe24b69afc4fb1
oharboe <oharboe@b42882b7-edfa-0310-969c-e2dbd0fdcd60>
2009-06-02 10:07:47 +0000

    more missing eCos types
    git-svn-id: svn://svn.berlios.de/openocd/trunk@2001 b42882b7-edfa-0310-969c-e2dbd0fdcd60

--- src/helper/types.h
+++ src/helper/types.h
@@ -139,6 +139,20 @@ static inline void h_u16_to_be(u8* buf, int val)
 #ifdef __ECOS
 /* eCos plain lacks this definition... */
 typedef CYG_ADDRWORD intptr_t;
+typedef int64_t intmax_t;
+typedef uint64_t uintmax_t;
+#define INT8_MAX 0x7f
+#define INT8_MIN (-INT8_MAX - 1)
+#define UINT8_MAX (__CONCAT(INT8_MAX, U) * 2U + 1U)
+#define INT16_MAX 0x7fff
+#define INT16_MIN (-INT16_MAX - 1)
+#define UINT16_MAX (__CONCAT(INT16_MAX, U) * 2U + 1U)
+#define INT32_MAX 0x7fffffffL
+#define INT32_MIN (-INT32_MAX - 1L)
+#define UINT32_MAX (__CONCAT(INT32_MAX, U) * 2UL + 1UL)
+#define INT64_MAX 0x7fffffffffffffffLL
+#define INT64_MIN (-INT64_MAX - 1LL)
+#define UINT64_MAX (__CONCAT(INT64_MAX, U) * 2ULL + 1ULL)
 #endif
 
 #endif /* TYPES_H */
