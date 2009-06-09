f7216ffa33bd91e88a2ca3dfcc50a0090310cddc
zwelch <zwelch@b42882b7-edfa-0310-969c-e2dbd0fdcd60>
2009-06-09 02:24:00 +0000

    Remove non-existant jtag_tap_by_position API declaration.
    git-svn-id: svn://svn.berlios.de/openocd/trunk@2145 b42882b7-edfa-0310-969c-e2dbd0fdcd60

--- src/jtag/jtag.h
+++ src/jtag/jtag.h
@@ -177,7 +177,6 @@ struct jtag_tap_s
 };
 extern jtag_tap_t* jtag_all_taps(void);
 extern const char *jtag_tap_name(const jtag_tap_t *tap);
-extern jtag_tap_t* jtag_tap_by_position(int n);
 extern jtag_tap_t* jtag_tap_by_string(const char* dotted_name);
 extern jtag_tap_t* jtag_tap_by_jim_obj(Jim_Interp* interp, Jim_Obj* obj);
 extern jtag_tap_t* jtag_tap_by_abs_position(unsigned abs_position);
