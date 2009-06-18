4cf8d5ec177dd05919a5a5eb32625bf7a4a3bb3f
zwelch <zwelch@b42882b7-edfa-0310-969c-e2dbd0fdcd60>
2009-06-18 07:11:48 +0000

    Remove redundant typedefs in types.h; include stdint.h unconditionally.
    git-svn-id: svn://svn.berlios.de/openocd/trunk@2283 b42882b7-edfa-0310-969c-e2dbd0fdcd60

--- src/helper/types.h
+++ src/helper/types.h
@@ -26,25 +26,7 @@
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
-#ifdef HAVE_STDINT_H
 #include <stdint.h>
-#endif
-
-#ifndef uint8_t
-typedef unsigned char uint8_t;
-#endif
-
-#ifndef uint16_t
-typedef unsigned short uint16_t;
-#endif
-
-#ifndef uint32_t
-typedef unsigned int uint32_t;
-#endif
-
-#ifndef uint64_t
-typedef unsigned long long uint64_t;
-#endif
 
 typedef struct jtag_tap_s jtag_tap_t;
 
