openocd-org/openocd

commit 0f6a47837e64836385268bb4e99e123477775e68

Simplify and improve handle_debug_level_comamnd: - Bug fix: Return a syntax error if more than one argument is given. - Bug fix: Use new parse_uint helper ensure debug_level parses correctly. - Change: Display the debug_level after it has been set. - Simplify bounds checking of debug_level.
git-svn-id: svn://svn.berlios.de/openocd/trunk@2208 b42882b7-edfa-0310-969c-e2dbd0fdcd60

src/helper/log.c
@@ -194,17 +194,18 @@ void log_printf_lf(enum log_levels level, const char *file, int line, const char
  */
 int handle_debug_level_command(struct command_context_s *cmd_ctx, char *cmd, char **args, int argc)
 {
-	if (argc == 0)
-		command_print(cmd_ctx, "debug_level: %i", debug_level);
+	if (argc == 1)
+	{
+		unsigned new_level;
+		int retval = parse_uint(args[0], &new_level);
+		if (ERROR_OK != retval)
+			return retval;
+		debug_level = MIN(new_level, LOG_LVL_DEBUG);
+	}
+	else if (argc > 1)
+		return ERROR_COMMAND_SYNTAX_ERROR;
 
-	if (argc > 0)
-		debug_level = strtoul(args[0], NULL, 0);
-
-	if (debug_level < 0)
-		debug_level = 0;
-
-	if (debug_level > 3)
-		debug_level = 3;
+	command_print(cmd_ctx, "debug_level: %i", debug_level);
 
 	if (debug_level >= LOG_LVL_DEBUG && server_use_pipes == 1)
 	{

commit 1d9ce8d2b3b77b7a59ac808a8913701de4250da6

- fix break caused by r2208 when using --pipe option - issue is gdb stdin buffer gets full before we redirect openocd output
git-svn-id: svn://svn.berlios.de/openocd/trunk@2350 b42882b7-edfa-0310-969c-e2dbd0fdcd60

src/helper/log.c
@@ -205,8 +205,6 @@ int handle_debug_level_command(struct command_context_s *cmd_ctx, char *cmd, cha
 	else if (argc > 1)
 		return ERROR_COMMAND_SYNTAX_ERROR;
 
-	command_print(cmd_ctx, "debug_level: %i", debug_level);
-
 	if (debug_level >= LOG_LVL_DEBUG && server_use_pipes == 1)
 	{
 		/* if we are enabling debug info then we need to write to a log file
@@ -218,6 +216,8 @@ int handle_debug_level_command(struct command_context_s *cmd_ctx, char *cmd, cha
 			LOG_WARNING("enabling log output as we are using pipes");
 		}
 	}
+
+	command_print(cmd_ctx, "debug_level: %i", debug_level);
 
 	return ERROR_OK;
 }