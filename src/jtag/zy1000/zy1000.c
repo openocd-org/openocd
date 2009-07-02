12c13acdf8761f4bc2931d07bce56f888f369876
oharboe <oharboe@b42882b7-edfa-0310-969c-e2dbd0fdcd60>
2009-07-02 09:11:18 +0000

    zy1000 1.53 snapshot
    git-svn-id: svn://svn.berlios.de/openocd/trunk@2451 b42882b7-edfa-0310-969c-e2dbd0fdcd60

--- src/jtag/zy1000/zy1000.c
+++ src/jtag/zy1000/zy1000.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2007-2008 by Øyvind Harboe                              *
+ *   Copyright (C) 2007-2008 by ï¿½yvind Harboe                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -28,7 +28,7 @@
 #include <cyg/hal/hal_diag.h>
 
 
-#define ZYLIN_VERSION "1.52"
+#define ZYLIN_VERSION "1.53"
 #define ZYLIN_DATE __DATE__
 #define ZYLIN_TIME __TIME__
 #define ZYLIN_OPENOCD "$Revision$"
