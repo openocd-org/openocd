f93c98081f58f1487d518356a05edf219b0237c3
Zachary T Welch <zw@superlucidity.net>
2009-11-11 07:19:13 -0800

    wrap help command
    Use continuation characters to wrap the help command into 80 columns.

--- src/helper/startup.tcl
+++ src/helper/startup.tcl
@@ -49,7 +49,10 @@ proc help {args} {
 	global ocd_helptext
 	set cmd $args
 	foreach a [lsort $ocd_helptext] {
-		if {[string length $cmd]==0||[string first $cmd $a]!=-1||[string first $cmd [lindex $a 1]]!=-1} {
+		if {[string length $cmd] == 0 || \
+			[string first $cmd $a] != -1 || \
+			[string first $cmd [lindex $a 1]] != -1} \
+		{
 			set w 50
 			set cmdname [lindex $a 0]
 			set h [lindex $a 1]
@@ -57,19 +60,24 @@ proc help {args} {
 			while 1 {
 				if {$n > [string length $h]} {break}
 
-				set next_a [expr $n+$w]
-				if {[string length $h]>$n+$w} {
-					set xxxx [string range $h $n [expr $n+$w]]
-					for {set lastpos [expr [string length $xxxx]-1]} {$lastpos>=0&&[string compare [string range $xxxx $lastpos $lastpos] " "]!=0} {set lastpos [expr $lastpos-1]} {
+				set next_a [expr $n + $w]
+				if {[string length $h] > $n + $w} \
+				{
+					set xxxx [string range $h $n [expr $n + $w]]
+					for {set lastpos [expr [string length $xxxx] - 1]} \
+						{$lastpos >= 0 && [string compare \
+							[string range $xxxx $lastpos $lastpos] " "] != 0} \
+						{set lastpos [expr $lastpos - 1]} \
+					{
 					}
 					#set next_a -1
-					if {$lastpos!=-1} {
-						set next_a [expr $lastpos+$n+1]
+					if {$lastpos != -1} {
+						set next_a [expr $lastpos + $n + 1]
 					}
 				}
 
-
-				puts [format "%-25s %s" $cmdname [string range $h $n [expr $next_a-1]] ]
+				puts [format "%-25s %s" $cmdname \
+						[string range $h $n [expr $next_a-1]] ]
 				set cmdname ""
 				set n [expr $next_a]
 			}
