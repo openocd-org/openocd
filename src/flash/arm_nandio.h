2221a037447b61b8a9db500ba997e61dbe874d61
Dean Glazeski <dnglaze@gmail.com>
2009-11-16 12:47:45 -0600

    ARM NAND I/O header documentation update.
    Fixed the header file to properly specify the doxygen documentation for the
    items defined in it.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>

--- src/flash/arm_nandio.h
+++ src/flash/arm_nandio.h
@@ -1,20 +1,24 @@
 #ifndef __ARM_NANDIO_H
-#define  __ARM_NANDIO_H
+#define __ARM_NANDIO_H
 
 #include "nand.h"
 #include "binarybuffer.h"
 
+/**
+ * The arm_nand_data struct is used for defining NAND I/O operations on an ARM
+ * core.
+ */
 struct arm_nand_data {
-	/* target is proxy for some ARM core */
+	/** target is proxy for some ARM core */
 	struct target		*target;
 
-	/* copy_area holds write-to-NAND loop and data to write */
+	/** copy_area holds write-to-NAND loop and data to write */
 	struct working_area	*copy_area;
 
-	/* chunk_size == page or ECC unit */
+	/** chunk_size == page or ECC unit */
 	unsigned		chunk_size;
 
-	/* data == where to write the data */
+	/** data == where to write the data */
 	uint32_t		data;
 
 	/* currently implicit:  data width == 8 bits (not 16) */
@@ -23,4 +27,4 @@ struct arm_nand_data {
 int arm_nandwrite(struct arm_nand_data *nand, uint8_t *data, int size);
 int arm_nandread(struct arm_nand_data *nand, uint8_t *data, uint32_t size);
 
-#endif  /* __ARM_NANDIO_H */
+#endif /* __ARM_NANDIO_H */
