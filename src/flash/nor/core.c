5fdee60fd4d38e59c7b5f7aca5ad50b90e7d61ee
Zachary T Welch <zw@superlucidity.net>
2009-12-04 04:37:27 -0800

    eliminate src/flash/flash.c
    Move remaining NOR flash implemenation into flash/nor/core.c
    Removes flash.c from the build, leaving only its header to split.

--- src/flash/nor/core.c
+++ src/flash/nor/core.c
@@ -1,4 +1,7 @@
 /***************************************************************************
+ *   Copyright (C) 2005 by Dominic Rath <Dominic.Rath@gmx.de>              *
+ *   Copyright (C) 2007,2008 Øyvind Harboe <oyvind.harboe@zylin.com>       *
+ *   Copyright (C) 2008 by Spencer Oliver <spen@spen-soft.co.uk>           *
  *   Copyright (C) 2009 Zachary T Welch <zw@superlucidity.net>             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -20,12 +23,13 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
-#include <flash/flash.h>
+#include <flash/common.h>
+#include <flash/nor/core.h>
 #include <flash/nor/imp.h>
 #include <target/image.h>
 
-// in flash.c, to be moved here
-extern struct flash_bank *flash_banks;
+
+struct flash_bank *flash_banks;
 
 int flash_driver_erase(struct flash_bank *bank, int first, int last)
 {
@@ -68,7 +72,6 @@ int flash_driver_write(struct flash_bank *bank,
 	return retval;
 }
 
-
 void flash_bank_add(struct flash_bank *bank)
 {
 	/* put flash bank in linked list */
@@ -96,6 +99,186 @@ struct flash_bank *flash_bank_list(void)
 	return flash_banks;
 }
 
+struct flash_bank *get_flash_bank_by_num_noprobe(int num)
+{
+	struct flash_bank *p;
+	int i = 0;
+
+	for (p = flash_banks; p; p = p->next)
+	{
+		if (i++ == num)
+		{
+			return p;
+		}
+	}
+	LOG_ERROR("flash bank %d does not exist", num);
+	return NULL;
+}
+
+int flash_get_bank_count(void)
+{
+	struct flash_bank *p;
+	int i = 0;
+	for (p = flash_banks; p; p = p->next)
+	{
+		i++;
+	}
+	return i;
+}
+
+struct flash_bank *get_flash_bank_by_name(const char *name)
+{
+	unsigned requested = get_flash_name_index(name);
+	unsigned found = 0;
+
+	struct flash_bank *bank;
+	for (bank = flash_banks; NULL != bank; bank = bank->next)
+	{
+		if (strcmp(bank->name, name) == 0)
+			return bank;
+		if (!flash_driver_name_matches(bank->driver->name, name))
+			continue;
+		if (++found < requested)
+			continue;
+		return bank;
+	}
+	return NULL;
+}
+
+struct flash_bank *get_flash_bank_by_num(int num)
+{
+	struct flash_bank *p = get_flash_bank_by_num_noprobe(num);
+	int retval;
+
+	if (p == NULL)
+		return NULL;
+
+	retval = p->driver->auto_probe(p);
+
+	if (retval != ERROR_OK)
+	{
+		LOG_ERROR("auto_probe failed %d\n", retval);
+		return NULL;
+	}
+	return p;
+}
+
+/* lookup flash bank by address */
+struct flash_bank *get_flash_bank_by_addr(struct target *target, uint32_t addr)
+{
+	struct flash_bank *c;
+
+	/* cycle through bank list */
+	for (c = flash_banks; c; c = c->next)
+	{
+		int retval;
+		retval = c->driver->auto_probe(c);
+
+		if (retval != ERROR_OK)
+		{
+			LOG_ERROR("auto_probe failed %d\n", retval);
+			return NULL;
+		}
+		/* check whether address belongs to this flash bank */
+		if ((addr >= c->base) && (addr <= c->base + (c->size - 1)) && target == c->target)
+			return c;
+	}
+	LOG_ERROR("No flash at address 0x%08" PRIx32 "\n", addr);
+	return NULL;
+}
+
+int default_flash_mem_blank_check(struct flash_bank *bank)
+{
+	struct target *target = bank->target;
+	const int buffer_size = 1024;
+	int i;
+	uint32_t nBytes;
+	int retval = ERROR_OK;
+
+	if (bank->target->state != TARGET_HALTED)
+	{
+		LOG_ERROR("Target not halted");
+		return ERROR_TARGET_NOT_HALTED;
+	}
+
+	uint8_t *buffer = malloc(buffer_size);
+
+	for (i = 0; i < bank->num_sectors; i++)
+	{
+		uint32_t j;
+		bank->sectors[i].is_erased = 1;
+
+		for (j = 0; j < bank->sectors[i].size; j += buffer_size)
+		{
+			uint32_t chunk;
+			chunk = buffer_size;
+			if (chunk > (j - bank->sectors[i].size))
+			{
+				chunk = (j - bank->sectors[i].size);
+			}
+
+			retval = target_read_memory(target, bank->base + bank->sectors[i].offset + j, 4, chunk/4, buffer);
+			if (retval != ERROR_OK)
+			{
+				goto done;
+			}
+
+			for (nBytes = 0; nBytes < chunk; nBytes++)
+			{
+				if (buffer[nBytes] != 0xFF)
+				{
+					bank->sectors[i].is_erased = 0;
+					break;
+				}
+			}
+		}
+	}
+
+	done:
+	free(buffer);
+
+	return retval;
+}
+
+int default_flash_blank_check(struct flash_bank *bank)
+{
+	struct target *target = bank->target;
+	int i;
+	int retval;
+	int fast_check = 0;
+	uint32_t blank;
+
+	if (bank->target->state != TARGET_HALTED)
+	{
+		LOG_ERROR("Target not halted");
+		return ERROR_TARGET_NOT_HALTED;
+	}
+
+	for (i = 0; i < bank->num_sectors; i++)
+	{
+		uint32_t address = bank->base + bank->sectors[i].offset;
+		uint32_t size = bank->sectors[i].size;
+
+		if ((retval = target_blank_check_memory(target, address, size, &blank)) != ERROR_OK)
+		{
+			fast_check = 0;
+			break;
+		}
+		if (blank == 0xFF)
+			bank->sectors[i].is_erased = 1;
+		else
+			bank->sectors[i].is_erased = 0;
+		fast_check = 1;
+	}
+
+	if (!fast_check)
+	{
+		LOG_USER("Running slow fallback erase check - add working memory");
+		return default_flash_mem_blank_check(bank);
+	}
+
+	return ERROR_OK;
+}
 /* erase given flash region, selects proper bank according to target and address */
 static int flash_iterate_address_range(struct target *target, uint32_t addr, uint32_t length,
 		int (*callback)(struct flash_bank *bank, int first, int last))
