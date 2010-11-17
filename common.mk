d80fca527a6618595a118381a2cd2a569f85889f
Spencer Oliver <ntfreak@users.sourceforge.net>
2010-11-17 10:26:21 +0000

    build: add autobuild jimtcl to configure scripts
    Rather than having to configure/build jimtcl openocd
    will do this as part of its own build.
    
    To use an external jimtcl lib specify disable-internal-jimtcl
    to the configure step.
    
    Signed-off-by: Spencer Oliver <ntfreak@users.sourceforge.net>

--- common.mk
+++ common.mk
@@ -3,3 +3,8 @@
 AM_CPPFLAGS = -I$(top_srcdir)/src \
 			  -I$(top_builddir)/src \
 			  -DPKGDATADIR=\"$(pkgdatadir)\"
+
+if INTERNAL_JIMTCL
+AM_CPPFLAGS += -I$(top_srcdir)/jimtcl \
+			   -I$(top_builddir)/jimtcl
+endif
