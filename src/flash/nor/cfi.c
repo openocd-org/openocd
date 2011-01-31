openocd-org/openocd

commit aa61a3b3d8b6acad19050987835ec05f3d298bdb

cfi: use safer arch detection
Signed-off-by: Spencer Oliver <ntfreak@users.sourceforge.net>

src/flash/nor/cfi.c
@@ -29,6 +29,7 @@
 #include "cfi.h"
 #include "non_cfi.h"
 #include <target/arm.h>
+#include <target/arm7_9_common.h>
 #include <target/armv7m.h>
 #include <helper/binarybuffer.h>
 #include <target/algorithm.h>
@@ -1644,17 +1645,22 @@ static int cfi_spansion_write_block(struct flash_bank *bank, uint8_t *buffer,
 		0xeafffffe		/* b	8204 <sp_8_done>		*/
 	};
 
-	if(strcmp("cortex_m3", target_type_name(target)) == 0) /* Cortex-M3 target */
+	if (is_armv7m(target_to_armv7m(target))) /* Cortex-M3 target */
 	{
 		armv4_5_info.common_magic = ARMV7M_COMMON_MAGIC;
 		armv4_5_info.core_mode = ARMV7M_MODE_HANDLER;
 		armv4_5_info.core_state = ARM_STATE_ARM;
 	}
-	else /* right now is only armv4_5 target */
+	else if (is_arm7_9(target_to_arm7_9(target)))
 	{
 		armv4_5_info.common_magic = ARM_COMMON_MAGIC;
 		armv4_5_info.core_mode = ARM_MODE_SVC;
 		armv4_5_info.core_state = ARM_STATE_ARM;
+	}
+	else
+	{
+		/* fallback to slow writes */
+		return ERROR_TARGET_RESOURCE_NOT_AVAILABLE;
 	}
 
 	int target_code_size;

commit a0858bfed053fa4555d729554ad8b8089a7ac308

cfi: use ARM32 machine code on all CPUs but Cortex M3
ARM11 broke with aa61a3b3d8b6acad19050987835ec05f3d298bdb
as the code only checked for arm 7/9.

CFI probably needs work for non-ARM targets but perhaps
not adding working area memory to e.g. MIPS will give
the default slow CFI support.

Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

src/flash/nor/cfi.c
@@ -1651,16 +1651,12 @@ static int cfi_spansion_write_block(struct flash_bank *bank, uint8_t *buffer,
 		armv4_5_info.core_mode = ARMV7M_MODE_HANDLER;
 		armv4_5_info.core_state = ARM_STATE_ARM;
 	}
-	else if (is_arm7_9(target_to_arm7_9(target)))
+	else
 	{
+		/* All other ARM CPUs have 32 bit instructions */
 		armv4_5_info.common_magic = ARM_COMMON_MAGIC;
 		armv4_5_info.core_mode = ARM_MODE_SVC;
 		armv4_5_info.core_state = ARM_STATE_ARM;
-	}
-	else
-	{
-		/* fallback to slow writes */
-		return ERROR_TARGET_RESOURCE_NOT_AVAILABLE;
 	}
 
 	int target_code_size = 0;