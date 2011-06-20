1aba8205191a4f5af1aa7aa3465a8abc86f4f9d5
Martin Schmölzer <martin.schmoelzer@student.tuwien.ac.at>
2011-06-20 19:51:46 +0200

    Add -DPKGLIBDIR to AM_CPPFLAGS.
    This is required so the OpenULINK driver knows the install location of the OpenULINK
    firmware image.

--- common.mk
+++ common.mk
@@ -2,7 +2,8 @@
 # common flags used in openocd build
 AM_CPPFLAGS = -I$(top_srcdir)/src \
 			  -I$(top_builddir)/src \
-			  -DPKGDATADIR=\"$(pkgdatadir)\"
+			  -DPKGDATADIR=\"$(pkgdatadir)\" \
+			  -DPKGLIBDIR=\"$(pkglibdir)\"
 
 if INTERNAL_JIMTCL
 AM_CPPFLAGS += -I$(top_srcdir)/jimtcl \
