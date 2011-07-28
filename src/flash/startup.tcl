852289bc4935ec8ac27242812fccb4806e304859
Spencer Oliver <ntfreak@users.sourceforge.net>
2011-07-28 12:47:49 +0100

    flash: add support for deprecated stm32 flash cmds
    Issue warning when the old cmd is used and redirect to new supported one.
    These deprecated cmds will be removed at some point.
    
    Signed-off-by: Spencer Oliver <ntfreak@users.sourceforge.net>

--- src/flash/startup.tcl
+++ src/flash/startup.tcl
@@ -1,2 +1,13 @@
 # Defines basic Tcl procs for OpenOCD flash module
 
+# ease migration to updated flash driver
+proc stm32x args {
+	echo "DEPRECATED! use 'stm32f1x $args' not 'stm32x $args'"
+	eval stm32f1x $args
+}
+
+proc stm32f2xxx args {
+	echo "DEPRECATED! use 'stm32f2x $args' not 'stm32f2xxx $args'"
+	eval stm32f2x $args
+}
+
