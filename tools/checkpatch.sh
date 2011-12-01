122509a0dc2e33b0526bdb6d16511489fa87ffe0
Andreas Fritiofson <andreas.fritiofson@gmail.com>
2011-12-01 21:25:43 +0100

    make checkpatch.sh take an optional 'since' refspec
    Change-Id: I793778037db08bd5462f61b9bcafd484708cc1b6
    Signed-off-by: Andreas Fritiofson <andreas.fritiofson@gmail.com>
    Reviewed-on: http://openocd.zylin.com/250
    Tested-by: jenkins
    Reviewed-by: Spencer Oliver <spen@spen-soft.co.uk>

--- tools/checkpatch.sh
+++ tools/checkpatch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #
 
-git format-patch -M --stdout HEAD^ | tools/scripts/checkpatch.pl - --no-tree
+since=${1:-HEAD^}
+git format-patch -M --stdout $since | tools/scripts/checkpatch.pl - --no-tree
