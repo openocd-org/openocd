7655e15ea53322b5ba7d19a3b02fb533bd849d93
Spencer Oliver <spen@spen-soft.co.uk>
2013-06-06 09:53:26 +0100

    stm32: add new stm32f0 device id
    updated from RM0091 rev4.
    
    Change-Id: Ic5e46229b85ce3974ef3016724d29a94037ac577
    Signed-off-by: Spencer Oliver <spen@spen-soft.co.uk>
    Reviewed-on: http://openocd.zylin.com/1435
    Tested-by: jenkins

--- src/flash/nor/stm32f1x.c
+++ src/flash/nor/stm32f1x.c
@@ -927,6 +927,7 @@ static int stm32x_probe(struct flash_bank *bank)
 		stm32x_info->default_rdp = 0x55AA;
 		break;
 	case 0x440: /* stm32f0x */
+	case 0x444:
 		page_size = 1024;
 		stm32x_info->ppage_size = 4;
 		max_flash_size_in_kb = 64;
@@ -1194,7 +1195,8 @@ static int get_stm32x_info(struct flash_bank *bank, char *buf, int buf_size)
 				snprintf(buf, buf_size, "unknown");
 				break;
 		}
-	} else if ((device_id & 0xfff) == 0x440) {
+	} else if (((device_id & 0xfff) == 0x440) ||
+			((device_id & 0xfff) == 0x444)) {
 		printed = snprintf(buf, buf_size, "stm32f0x - Rev: ");
 		buf += printed;
 		buf_size -= printed;
