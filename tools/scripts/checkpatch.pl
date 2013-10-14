7bf39f64f64959a3fea60e74a26c9b54e4ab0f0a
Paul Fertser <fercerpav@gmail.com>
2013-10-14 10:03:00 +0400

    checkpatch: add logging functions
    It's commonly considered that user-visible strings should not be split
    to different lines in the sources to ease grepping for them. Hence,
    checkpatch traditionally makes an exception for logging functions,
    lines having them can be of arbitrary length.
    
    OpenOCD uses different (from Linux, the kernel) names, so they need to
    be added to avoid false positives.
    
    This is the old commit bb3cd6ec438d ("checkpatch: add logging functions")
    re-applied and updated.
    
    Change-Id: Ib18e4532cf7e1f79821b74c9bb6134a8a4e8be1b
    Signed-off-by: Paul Fertser <fercerpav@gmail.com>
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: https://review.openocd.org/c/openocd/+/5119
    Tested-by: jenkins

--- tools/scripts/checkpatch.pl
+++ tools/scripts/checkpatch.pl
@@ -597,6 +597,7 @@ our $typeTypedefs = qr{(?x:
 
 our $zero_initializer = qr{(?:(?:0[xX])?0+$Int_type?|NULL|false)\b};
 
+if (!$OpenOCD) {
 our $logFunctions = qr{(?x:
 	printk(?:_ratelimited|_once|_deferred_once|_deferred|)|
 	(?:[a-z0-9]+_){1,2}(?:printk|emerg|alert|crit|err|warning|warn|notice|info|debug|dbg|vdbg|devel|cont|WARN)(?:_ratelimited|_once|)|
@@ -606,6 +607,12 @@ our $logFunctions = qr{(?x:
 	MODULE_[A-Z_]+|
 	seq_vprintf|seq_printf|seq_puts
 )};
+} # !$OpenOCD
+# OpenOCD specific: Begin: list log functions
+our $logFunctions = qr{(?x:
+	LOG_(?:TARGET_|)(?:DEBUG_IO|DEBUG|INFO|WARNING|ERROR|USER|USER_N|OUTPUT)
+)};
+# OpenOCD specific: End
 
 our $allocFunctions = qr{(?x:
 	(?:(?:devm_)?
