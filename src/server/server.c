558279c1bb6fb2e39b68ac5020f5cb8271e54a9d
Paul Fertser <fercerpav@gmail.com>
2014-02-06 14:03:02 +0400

    server: fix confusing wording for incoming tcp connections
    Change-Id: I40d5de322f3fc38097e04ce538b0fc2b136e0d6a
    Signed-off-by: Paul Fertser <fercerpav@gmail.com>
    Reviewed-on: http://openocd.zylin.com/1937
    Tested-by: jenkins
    Reviewed-by: Spencer Oliver <spen@spen-soft.co.uk>
    Reviewed-by: Andreas Fritiofson <andreas.fritiofson@gmail.com>

--- src/server/server.c
+++ src/server/server.c
@@ -80,7 +80,7 @@ static int add_connection(struct service *service, struct command_context *cmd_c
 			(char *)&flag,			/* the cast is historical cruft */
 			sizeof(int));			/* length of option value */
 
-		LOG_INFO("accepting '%s' connection from %s", service->name, service->port);
+		LOG_INFO("accepting '%s' connection on tcp/%s", service->name, service->port);
 		retval = service->new_connection(c);
 		if (retval != ERROR_OK) {
 			close_socket(c->fd);
