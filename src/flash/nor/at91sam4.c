a769be6b9c9dea7e498535eba0a0255f1e785575
Evan Hunter <ehunter@broadcom.com>
2015-07-17 13:22:06 +0100

    flash : Add support for Atmel at91sam4sa16b
    Change-Id: Ief6833b4bf587fbf53c8fbeee2fc276a95ca0a8a
    Signed-off-by: Evan Hunter <ehunter@broadcom.com>
    Reviewed-on: http://openocd.zylin.com/2878
    Tested-by: jenkins
    Reviewed-by: Spencer Oliver <spen@spen-soft.co.uk>

--- src/flash/nor/at91sam4.c
+++ src/flash/nor/at91sam4.c
@@ -363,6 +363,39 @@ static const struct sam4_chip_details all_sam4_details[] = {
 		  },
 		},
 	},
+	/*atsam4sa16b - LQFP64/QFN64*/
+	{
+		.chipid_cidr    = 0x28970CE0,
+		.name           = "at91sam4sa16b",
+		.total_flash_size     = 1024 * 1024,
+		.total_sram_size      = 160 * 1024,
+		.n_gpnvms       = 2,
+		.n_banks        = 1,
+		{
+/*		.bank[0] = {*/
+		  {
+			.probed = 0,
+			.pChip  = NULL,
+			.pBank  = NULL,
+			.bank_number = 0,
+			.base_address = FLASH_BANK_BASE_S,
+			.controller_address = 0x400e0a00,
+			.flash_wait_states = 6,	/* workaround silicon bug */
+			.present = 1,
+			.size_bytes =  1024 * 1024,
+			.nsectors   =  128,
+			.sector_size = 8192,
+			.page_size   = 512,
+		  },
+/*		.bank[1] = {*/
+		  {
+			.present = 0,
+			.probed = 0,
+			.bank_number = 1,
+
+		  },
+		},
+	},
 	/*atsam4s16a - LQFP48/QFN48*/
 	{
 		.chipid_cidr    = 0x288C0CE0,
