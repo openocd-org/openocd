9ad6ebc89a702a3af8947605800b2eeb816e9ba7
Andreas Färber <afaerber@suse.de>
2016-05-14 13:59:40 +0200

    arm_adi_v5: Reorder Atmel part number entry
    Instead of placing Atmel last, after ANY_ID, place it after ARM (it's
    arm_adi_v5 despite 0x4BB) and sort it with the other vendors, i.e.
    before ADI and Qualcomm. Adapt column alignment.
    
    Drop the redundant "Atmel" comment to clarify that Analog is not Atmel.
    
    Change-Id: Ic06785db079cf58d49815a639236636c180e5e17
    Signed-off-by: Andreas Färber <afaerber@suse.de>
    Reviewed-on: http://openocd.zylin.com/3479
    Tested-by: jenkins
    Reviewed-by: Freddie Chopin <freddie.chopin@gmail.com>

--- src/target/arm_adi_v5.c
+++ src/target/arm_adi_v5.c
@@ -1039,13 +1039,12 @@ static const struct {
 	{ ARM_ID, 0xd03, "Cortex-A53 Debug",  "(Debug Unit)", },
 	{ ARM_ID, 0xd07, "Cortex-A57 Debug",  "(Debug Unit)", },
 	{ ARM_ID, 0xd08, "Cortex-A72 Debug",  "(Debug Unit)", },
+	{ 0x09f,  0xcd0, "Atmel CPU with DSU", "(CPU)" },
 	{ 0x0E5,  0x000, "SHARC+/Blackfin+", "", },
 	{ 0x0F0,  0x440, "Qualcomm QDSS Component v1", "(Qualcomm Designed CoreSight Component v1)", },
 	/* legacy comment: 0x113: what? */
 	{ ANY_ID,  0x120, "TI SDTI",         "(System Debug Trace Interface)", }, /* from OMAP3 memmap */
 	{ ANY_ID,  0x343, "TI DAPCTL",       "", }, /* from OMAP3 memmap */
-	/* Atmel */
-	{ 0x09f, 0xcd0, "Atmel CPU with DSU", "(CPU)" },
 };
 
 static int dap_rom_display(struct command_context *cmd_ctx,
