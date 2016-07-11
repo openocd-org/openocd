7a0473bbb6cd4d83ce43fc4b722083b315b80a98
Matthias Welwarsky <matthias.welwarsky@sysgo.com>
2016-07-11 15:23:42 +0200

    tcl: add mrb command to mem_helper.tcl
    add "mrb" command to read a byte of memory into a variable
    
    Change-Id: I5ddc9fbcc55958a249548627bd15824df6dc0d61
    Signed-off-by: Matthias Welwarsky <matthias.welwarsky@sysgo.com>
    Reviewed-on: http://openocd.zylin.com/3542
    Tested-by: jenkins
    Reviewed-by: Andreas Fritiofson <andreas.fritiofson@gmail.com>

--- tcl/mem_helper.tcl
+++ tcl/mem_helper.tcl
@@ -10,6 +10,15 @@ proc mrw {reg} {
 add_usage_text mrw "address"
 add_help_text mrw "Returns value of word in memory."
 
+proc mrb {reg} {
+	set value ""
+	mem2array value 8 $reg 1
+	return $value(0)
+}
+
+add_usage_text mrb "address"
+add_help_text mrb "Returns value of byte in memory."
+
 # mmw: "memory modify word", updates value of $reg
 #       $reg <== ((value & ~$clearbits) | $setbits)
 proc mmw {reg setbits clearbits} {
