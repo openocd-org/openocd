ef4c139ce224e4d3784bcff411604c1908b13da6
Andreas Fritiofson <andreas.fritiofson@gmail.com>
2016-11-06 15:49:35 +0100

    configure: Don't set user variables
    CFLAGS is a user variable and shouldn't be set in either
    configure.ac or Makefile.am because that makes it impossible to
    override compiler flags at configure or make time.
    
    AC_SUBST the default warning flags into AM_CFLAGS instead, which
    is emitted before CFLAGS on the compiler command line.
    
    Do the same thing with a mingw-specific flag as well.
    
    Change-Id: I6ac3a3c8b7e0abf6f0e60ea239b549d68ea6d370
    Signed-off-by: Andreas Fritiofson <andreas.fritiofson@gmail.com>
    Reviewed-on: http://openocd.zylin.com/3870
    Tested-by: jenkins
    Reviewed-by: Paul Fertser <fercerpav@gmail.com>

--- common.mk
+++ common.mk
@@ -1,6 +1,9 @@
 
 # common flags used in openocd build
-AM_CPPFLAGS = -I$(top_srcdir)/src \
+AM_CFLAGS = $(GCC_WARNINGS)
+
+AM_CPPFLAGS = $(HOST_CPPFLAGS)\
+			  -I$(top_srcdir)/src \
 			  -I$(top_builddir)/src \
 			  -I$(top_srcdir)/src/helper \
 			  -DPKGDATADIR=\"$(pkgdatadir)\" \
