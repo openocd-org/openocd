6f9a7eca7144c9a84df7a7113e84047dab600c51
Matthias Welwarsky <matthias.welwarsky@sysgo.com>
2016-11-11 14:39:09 +0100

    Add -defer-examine option to target create command
    The '-defer-examine' option to target create allows declaring targets
    that are present on the chain, but not fully functional.  They will
    be skipped by the initial arp_examine as well as arp_examine after
    reset.
    
    Manual examine using 'arp_examine' is needed to examine them, with the
    idea that some kind of actions is neeed to bring them to a state where
    examine will succeed (if at all possible).
    
    In order to allow value less options to target command, I had to relax
    the goi.argc check in jim_target_configure().
    
    Change-Id: I9bf4e8d27eb6476dd9353d15f48965a8cfd5c122
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>
    Signed-off-by: Matthias Welwarsky <matthias.welwarsky@sysgo.com>

--- src/rtos/rtos.c
+++ src/rtos/rtos.c
@@ -104,6 +104,7 @@ int rtos_create(Jim_GetOptInfo *goi, struct target *target)
 	int x;
 	const char *cp;
 	struct Jim_Obj *res;
+	int e;
 
 	if (!goi->isconfigure && goi->argc != 0) {
 		Jim_WrongNumArgs(goi->interp, goi->argc, goi->argv, "NO PARAMS");
@@ -112,7 +113,9 @@ int rtos_create(Jim_GetOptInfo *goi, struct target *target)
 
 	os_free(target);
 
-	Jim_GetOpt_String(goi, &cp, NULL);
+	e = Jim_GetOpt_String(goi, &cp, NULL);
+	if (e != JIM_OK)
+		return e;
 
 	if (0 == strcmp(cp, "auto")) {
 		/* Auto detect tries to look up all symbols for each RTOS,
