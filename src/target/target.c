5d458cf72734a4474f38bbed10eea4d9acfe93a2
Matthias Welwarsky <matthias.welwarsky@sysgo.com>
2017-02-20 14:29:01 +0100

    target/mem_ap: generic mem-ap target
    This pseudo target allows attaching to any access point on the DAP at the
    MEM-AP level and read and write addresses on the connected bus. For
    example, one can create a mem_ap target on the APB-AP and read and write
    registers of debug components directly. This allows many diagnostic
    and other features be programmed entirely using TCL, without necessity
    of adding drivers to OpenOCD.
    
    Change-Id: I53229ffd68fb0f96fb68be15b0f3a76cc8843c8e
    Signed-off-by: Matthias Welwarsky <matthias.welwarsky@sysgo.com>
    Reviewed-on: http://openocd.zylin.com/4002
    Tested-by: jenkins
    Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-by: Leonard Crestez <cdleonard@gmail.com>

--- src/target/target.c
+++ src/target/target.c
@@ -108,6 +108,7 @@ extern struct target_type quark_x10xx_target;
 extern struct target_type quark_d20xx_target;
 extern struct target_type stm8_target;
 extern struct target_type riscv_target;
+extern struct target_type mem_ap_target;
 
 static struct target_type *target_types[] = {
 	&arm7tdmi_target,
@@ -144,6 +145,7 @@ static struct target_type *target_types[] = {
 #if BUILD_TARGET64
 	&aarch64_target,
 #endif
+	&mem_ap_target,
 	NULL,
 };
 
