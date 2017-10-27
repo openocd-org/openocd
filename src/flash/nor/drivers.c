deaf3d264123391d8fe5c4cccbf8fb8852e1be23
Tomas Vanek <vanekt@fbl.cz>
2017-10-27 21:43:25 +0200

    flash/nor: flash driver and cfg for SAM E54, E53, E51 and D51
    The new Microchip (former Atmel) series powered by Cortex-M4 looks
    very similar to older M0+ powered SAM D2x at the first sight.
    Unfortunately the new series differs a lot in important details.
    NVMCTRL has different register addresses, moved important bits
    and even changed binary command set. An universal driver for all SAM D/E
    would be very complicated. That's why a new driver was derived.
    
    Tested on Microchip SAM E54 Xplained Pro kit (board cfg included).
    
    Adjusted for the restructured dap support.
    Checked by valgrind and clang static analyzer.
    
    Change-Id: I26c67047a552076f4b207b9b89285a53d69b4ca4
    Signed-off-by: Tomas Vanek <vanekt@fbl.cz>
    Reviewed-on: http://openocd.zylin.com/4272
    Tested-by: jenkins
    Reviewed-by: Andres Vahter <andres.vahter@gmail.com>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -29,6 +29,7 @@ extern struct flash_driver at91sam4l_flash;
 extern struct flash_driver at91sam7_flash;
 extern struct flash_driver at91samd_flash;
 extern struct flash_driver ath79_flash;
+extern struct flash_driver atsame5_flash;
 extern struct flash_driver atsamv_flash;
 extern struct flash_driver avr_flash;
 extern struct flash_driver bluenrgx_flash;
@@ -97,6 +98,7 @@ static struct flash_driver *flash_drivers[] = {
 	&at91sam7_flash,
 	&at91samd_flash,
 	&ath79_flash,
+	&atsame5_flash,
 	&atsamv_flash,
 	&avr_flash,
 	&bluenrgx_flash,
