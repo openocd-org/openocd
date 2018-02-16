a4cdce0129a6b206a2081d77dd518ce2cae0b71f
Tomas Vanek <vanekt@fbl.cz>
2018-02-16 11:58:10 +0100

    gdb_server: prevent false positive valgrind report
    Change-Id: Ia59fdf8a23043889840122859b0c5bdb5f757703
    Signed-off-by: Tomas Vanek <vanekt@fbl.cz>
    Reviewed-on: http://openocd.zylin.com/4420
    Tested-by: jenkins

--- src/server/gdb_server.c
+++ src/server/gdb_server.c
@@ -2442,7 +2442,11 @@ static int gdb_get_thread_list_chunk(struct target *target, char **thread_list,
 	else
 		transfer_type = 'l';
 
-	*chunk = malloc(length + 2);
+	*chunk = malloc(length + 2 + 3);
+    /* Allocating extra 3 bytes prevents false positive valgrind report
+	 * of strlen(chunk) word access:
+	 * Invalid read of size 4
+	 * Address 0x4479934 is 44 bytes inside a block of size 45 alloc'd */
 	if (*chunk == NULL) {
 		LOG_ERROR("Unable to allocate memory");
 		return ERROR_FAIL;
