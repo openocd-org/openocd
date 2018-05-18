0ad060d97a36e4a5c2d7bf1862d5ad52f6de9e86
Tim Newsome <tim@sifive.com>
2018-05-17 18:08:08 -0700

    Review feedback.
    Change-Id: If58c011fc8d89d329d65a6c624ffb631f111cef2

--- src/target/riscv/riscv.c
+++ src/target/riscv/riscv.c
@@ -1766,7 +1766,7 @@ bool riscv_has_register(struct target *target, int hartid, int regid)
 /**
  * This function is called when the debug user wants to change the value of a
  * register. The new value may be cached, and may not be written until the hart
- * is running again. */
+ * is resumed. */
 int riscv_set_register(struct target *target, enum gdb_regno r, riscv_reg_t v)
 {
 	return riscv_set_register_on_hart(target, riscv_current_hartid(target), r, v);
