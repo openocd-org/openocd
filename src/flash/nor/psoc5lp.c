548bcee30dcfec3174e563e53ffb1559104b8a97
Antonio Borneo <borneo.antonio@gmail.com>
2018-06-11 10:29:52 +0200

    flash/nor/psoc5lp: fix compile issue on GCC 8.1.0
    Issue already identified by Alex https://sourceforge.net/u/alexbour/
    in ticket #191 https://sourceforge.net/p/openocd/tickets/191/
    
    	src/flash/nor/psoc5lp.c:237:2: error: ‘strncpy’ output
    	truncated before terminating nul copying 2 bytes from a
    	string of the same length [-Werror=stringop-truncation]
    
    Fix it by assigning the value to the array elements.
    
    Change-Id: I22468e5700efa64ea48ae8cdec930c48b4a7d8fb
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: http://openocd.zylin.com/4563
    Tested-by: jenkins
    Reviewed-by: Tomas Vanek <vanekt@fbl.cz>

--- src/flash/nor/psoc5lp.c
+++ src/flash/nor/psoc5lp.c
@@ -234,7 +234,8 @@ static void psoc5lp_get_part_number(const struct psoc5lp_device *dev, char *str)
 	}
 
 	/* Package does not matter. */
-	strncpy(str + 8, "xx", 2);
+	str[8] = 'x';
+	str[9] = 'x';
 
 	/* Temperate range cannot uniquely be identified. */
 	str[10] = 'x';
