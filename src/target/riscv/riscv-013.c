005cfca219e3784bedba436a124b712dd7120724
Tim Newsome <tim@sifive.com>
2018-11-05 11:50:09 -0800

    Install patchutils for the build. (#321)
    * Install patchutils for the build.
    
    This contains filterdiff, which we need to check that our changes
    conform to OpenOCD style.
    
    Change-Id: Id522f4e62fee3efad4e0e00933abfeada9635624
    
    * Fix paths for filterdiff line.
    
    Change-Id: Ic50e13c7fe64e65b2d2af0260fb19c07a9f10e20
    
    * Conform to OpenOCD style.
    
    Change-Id: I51660d30404c0a625b58c9bed2d948695575e72e

--- src/target/riscv/riscv-013.c
+++ src/target/riscv/riscv-013.c
@@ -3292,7 +3292,7 @@ static int riscv013_test_sba_config_reg(struct target *target,
 		LOG_INFO("ALL TESTS PASSED");
 		return ERROR_OK;
 	} else {
-		LOG_ERROR("%d TESTS FAILED", tests_failed);	
+		LOG_ERROR("%d TESTS FAILED", tests_failed);
 		return ERROR_FAIL;
 	}
 
@@ -3369,9 +3369,8 @@ static int maybe_execute_fence_i(struct target *target)
 {
 	RISCV013_INFO(info);
 	RISCV_INFO(r);
-	if (info->progbufsize + r->impebreak >= 3) {
+	if (info->progbufsize + r->impebreak >= 3)
 		return execute_fence(target);
-	}
 	return ERROR_OK;
 }
 
