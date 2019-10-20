3008756604b8b7a3811948cf66ed7e587efac349
Antonio Borneo <borneo.antonio@gmail.com>
2019-10-20 19:40:28 +0200

    helper/command: strip replicated command name in log
    After commit 0840414f0e57 ("helper/command: do not replace new
    commands with ocd_ prefix"), the command name is present in
    argv[0], so there is no need to pass it directly. The current code
    causes the command name to be logged twice, once explicitly and
    then from the content of argv[0]:
    	openocd -c 'debug_level 3; echo hello; shutdown'
    	<snip>
    	Debug: 13 3 command.c:142 script_debug(): command - echo echo hello
    	<snip>
    	Debug: 16 4 command.c:142 script_debug(): command - shutdown shutdown
    
    Remove the command name from the arguments of the function
    script_debug().
    
    Change-Id: I57860774f450ff717ee71ef9dc07590549a84319
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: http://openocd.zylin.com/5660
    Reviewed-by: Andreas Fritiofson <andreas.fritiofson@gmail.com>
    Tested-by: jenkins

--- src/jtag/tcl.c
+++ src/jtag/tcl.c
@@ -108,7 +108,7 @@ static int Jim_Command_drscan(Jim_Interp *interp, int argc, Jim_Obj *const *args
 
 	endstate = TAP_IDLE;
 
-	script_debug(interp, "drscan", argc, args);
+	script_debug(interp, argc, args);
 
 	/* validate arguments as numbers */
 	e = JIM_OK;
@@ -229,7 +229,7 @@ static int Jim_Command_pathmove(Jim_Interp *interp, int argc, Jim_Obj *const *ar
 		return JIM_ERR;
 	}
 
-	script_debug(interp, "pathmove", argc, args);
+	script_debug(interp, argc, args);
 
 	int i;
 	for (i = 0; i < argc-1; i++) {
@@ -261,7 +261,7 @@ static int Jim_Command_pathmove(Jim_Interp *interp, int argc, Jim_Obj *const *ar
 
 static int Jim_Command_flush_count(Jim_Interp *interp, int argc, Jim_Obj *const *args)
 {
-	script_debug(interp, "flush_count", argc, args);
+	script_debug(interp, argc, args);
 
 	Jim_SetResult(interp, Jim_NewIntObj(interp, jtag_get_flush_queue_count()));
 
