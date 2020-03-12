f00070edaf8fbe7e49c44845c440086d55288f28
Evgeniy Didin <didin@synopsys.com>
2020-03-12 19:05:37 +0300

    target/arc_cmd: Improve argument checks for commands
    Add more argument check for "add-reg" command.
    
    Changes since first revision:
    -Removed arguments limitation(50 maximum) for "arc_set_reg_exists".
    
    Changes:
    25.03:
    Removed inconsistency in "add-reg" function. Actually
    "-type" option is optional and if it is not set,
    register type is "int".
    
    Change-Id: Ia21e6baf4fbda162f7811cd0fe305fc86ddafcfd
    Signed-off-by: Evgeniy Didin <didin@synopsys.com>
    Reviewed-on: http://openocd.zylin.com/5523
    Tested-by: jenkins
    Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-by: Oleksij Rempel <linux@rempel-privat.de>

--- src/target/arc_cmd.c
+++ src/target/arc_cmd.c
@@ -678,13 +678,15 @@ static int jim_arc_add_reg(Jim_Interp *interp, int argc, Jim_Obj * const *argv)
 	int type_name_len = strlen(type_name);
 	int e = ERROR_OK;
 
-	/* At least we need to specify 4 parameters: name, number, type and gdb_feature,
-	 * which means there should be 8 arguments */
-	if (goi.argc < 8) {
+	/* At least we need to specify 4 parameters: name, number and gdb_feature,
+	 * which means there should be 6 arguments. Also there can be additional paramters
+	 * "-type <type>", "-g" and  "-core" or "-bcr" which makes maximum 10 parameters. */
+	if (goi.argc < 6 || goi.argc > 10) {
 		free_reg_desc(reg);
 		Jim_SetResultFormatted(goi.interp,
-			"Should be at least 8 argnuments: -name <name> "
-			"-num <num> -type <type> -feature <gdb_feature>.");
+			"Should be at least 6 argnuments and not greater than 10: "
+			" -name <name> -num <num> -feature <gdb_feature> "
+			" [-type <type_name>] [-core|-bcr] [-g].");
 		return JIM_ERR;
 	}
 
