4be5349c29f6d527a8e7cfc494bb3c7a962a3e7b
Antonio Borneo <borneo.antonio@gmail.com>
2020-05-13 16:51:00 +0200

    helper/command: make script_debug() static
    Now that all commands are executed through the common handler
    command_unknown(), the message about command execution is logged
    by command_unknown(). There is no need, for "native" commands
    (.jim_handler) at root level to log the message (again) by itself.
    
    Remove calls to script_debug() apart from command_unknown().
    Make script_debug() static as only used in command.c.
    
    Change-Id: I9b2728b69e7643d6121c4b35a96bc825bcb5488d
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/helper/command.c
+++ src/helper/command.c
@@ -146,7 +146,7 @@ extern struct command_context *global_cmd_ctx;
 
 /* dump a single line to the log for the command.
  * Do nothing in case we are not at debug level 3 */
-void script_debug(Jim_Interp *interp, unsigned int argc, Jim_Obj * const *argv)
+static void script_debug(Jim_Interp *interp, unsigned int argc, Jim_Obj * const *argv)
 {
 	if (debug_level < LOG_LVL_DEBUG)
 		return;
