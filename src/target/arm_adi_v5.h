ab337d05f432465fdfedb694ac862fb22f80d7a6
Tomas Vanek <vanekt@fbl.cz>
2021-04-06 13:46:07 +0200

    target/arm_adi_v5: move DP register definitions to one block
    DP and MEM-AP definitions were mixed.
    
    Change-Id: I2f691b2274c01e9090c1e5160c6903d3207e35c2
    Signed-off-by: Tomas Vanek <vanekt@fbl.cz>
    Reviewed-on: http://openocd.zylin.com/6138
    Tested-by: jenkins

--- src/target/arm_adi_v5.h
+++ src/target/arm_adi_v5.h
@@ -85,6 +85,15 @@
 #define CSYSPWRUPREQ    (1UL << 30)
 #define CSYSPWRUPACK    (1UL << 31)
 
+#define DP_SELECT_APSEL 0xFF000000
+#define DP_SELECT_APBANK 0x000000F0
+#define DP_SELECT_DPBANK 0x0000000F
+#define DP_SELECT_INVALID 0x00FFFF00 /* Reserved bits one */
+
+#define DP_APSEL_MAX        (255)
+#define DP_APSEL_INVALID    (-1)
+
+
 /* MEM-AP register addresses */
 #define MEM_AP_REG_CSW		0x00
 #define MEM_AP_REG_TAR		0x04
@@ -149,14 +158,6 @@
 #define IDR_TYPE    (0xFUL << 0)
 
 #define IDR_JEP106_ARM 0x04760000
-
-#define DP_SELECT_APSEL 0xFF000000
-#define DP_SELECT_APBANK 0x000000F0
-#define DP_SELECT_DPBANK 0x0000000F
-#define DP_SELECT_INVALID 0x00FFFF00 /* Reserved bits one */
-
-#define DP_APSEL_MAX        (255)
-#define DP_APSEL_INVALID    (-1)
 
 /* FIXME: not SWD specific; should be renamed, e.g. adiv5_special_seq */
 enum swd_special_seq {
