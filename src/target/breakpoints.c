f78f9a90a6c4f8abedbac8a4047631cbe3ea0e17
Tim Newsome <tim@sifive.com>
2021-09-01 14:25:10 -0700

    In SMP config, replicate watchpoints on each core
    This works well with gdb on RISC-V, since hardware breakpoints are
    per-core and gdb thinks that targets are really processes on a machine.
    
    Are there targets where this is a bad idea? Should the target definition
    specify whether this behavior is desired or not?
    
    Change-Id: Ia32be2707b04347fd8bf2ca6fbb2b0ceaad3704a
    Signed-off-by: Tim Newsome <tim@sifive.com>
    Reviewed-on: https://review.openocd.org/c/openocd/+/6528
    Tested-by: jenkins
    Reviewed-by: Oleksij Rempel <linux@rempel-privat.de>
    Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/target/breakpoints.c
+++ src/target/breakpoints.c
@@ -95,7 +95,9 @@ fail:
 			return retval;
 	}
 
-	LOG_DEBUG("added %s breakpoint at " TARGET_ADDR_FMT " of length 0x%8.8x, (BPID: %" PRIu32 ")",
+	LOG_DEBUG("[%d] added %s breakpoint at " TARGET_ADDR_FMT
+			" of length 0x%8.8x, (BPID: %" PRIu32 ")",
+		target->coreid,
 		breakpoint_type_strings[(*breakpoint_p)->type],
 		(*breakpoint_p)->address, (*breakpoint_p)->length,
 		(*breakpoint_p)->unique_id);
@@ -410,8 +412,8 @@ struct breakpoint *breakpoint_find(struct target *target, target_addr_t address)
 	return NULL;
 }
 
-int watchpoint_add(struct target *target, target_addr_t address, uint32_t length,
-	enum watchpoint_rw rw, uint32_t value, uint32_t mask)
+int watchpoint_add_internal(struct target *target, target_addr_t address,
+		uint32_t length, enum watchpoint_rw rw, uint32_t value, uint32_t mask)
 {
 	struct watchpoint *watchpoint = target->watchpoints;
 	struct watchpoint **watchpoint_p = &target->watchpoints;
@@ -466,14 +468,39 @@ bye:
 			return retval;
 	}
 
-	LOG_DEBUG("added %s watchpoint at " TARGET_ADDR_FMT
-		" of length 0x%8.8" PRIx32 " (WPID: %d)",
+	LOG_DEBUG("[%d] added %s watchpoint at " TARGET_ADDR_FMT
+			" of length 0x%8.8" PRIx32 " (WPID: %d)",
+		target->coreid,
 		watchpoint_rw_strings[(*watchpoint_p)->rw],
 		(*watchpoint_p)->address,
 		(*watchpoint_p)->length,
 		(*watchpoint_p)->unique_id);
 
 	return ERROR_OK;
+}
+
+int watchpoint_add(struct target *target, target_addr_t address,
+		uint32_t length, enum watchpoint_rw rw, uint32_t value, uint32_t mask)
+{
+	int retval = ERROR_OK;
+	if (target->smp) {
+		struct target_list *head;
+		struct target *curr;
+		head = target->head;
+
+		while (head != (struct target_list *)NULL) {
+			curr = head->target;
+			retval = watchpoint_add_internal(curr, address, length, rw, value,
+					mask);
+			if (retval != ERROR_OK)
+				return retval;
+			head = head->next;
+		}
+		return retval;
+	} else {
+		return watchpoint_add_internal(target, address, length, rw, value,
+				mask);
+	}
 }
 
 static void watchpoint_free(struct target *target, struct watchpoint *watchpoint_to_remove)
@@ -497,7 +524,7 @@ static void watchpoint_free(struct target *target, struct watchpoint *watchpoint
 	free(watchpoint);
 }
 
-void watchpoint_remove(struct target *target, target_addr_t address)
+int watchpoint_remove_internal(struct target *target, target_addr_t address)
 {
 	struct watchpoint *watchpoint = target->watchpoints;
 
@@ -507,10 +534,33 @@ void watchpoint_remove(struct target *target, target_addr_t address)
 		watchpoint = watchpoint->next;
 	}
 
-	if (watchpoint)
+	if (watchpoint) {
 		watchpoint_free(target, watchpoint);
-	else
-		LOG_ERROR("no watchpoint at address " TARGET_ADDR_FMT " found", address);
+		return 1;
+	} else {
+		if (!target->smp)
+			LOG_ERROR("no watchpoint at address " TARGET_ADDR_FMT " found", address);
+		return 0;
+	}
+}
+
+void watchpoint_remove(struct target *target, target_addr_t address)
+{
+	if (target->smp) {
+		unsigned int num_watchpoints = 0;
+		struct target_list *head;
+		struct target *curr;
+		head = target->head;
+		while (head) {
+			curr = head->target;
+			num_watchpoints += watchpoint_remove_internal(curr, address);
+			head = head->next;
+		}
+		if (num_watchpoints == 0)
+			LOG_ERROR("no watchpoint at address " TARGET_ADDR_FMT " num_watchpoints", address);
+	} else {
+		watchpoint_remove_internal(target, address);
+	}
 }
 
 void watchpoint_clear_target(struct target *target)
