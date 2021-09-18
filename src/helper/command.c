openocd-org/openocd

commit 4747af362de04b508c91df4004d2aed5579c8a1c

JIM: document "echo" command
Document "-n" option in manual;
Modify "echo" command definition as COMMAND_HANDLER to
easily add help message
Add help message aligned with manual.

Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

src/helper/command.c
@@ -777,19 +777,16 @@ static int jim_find(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
 	return JIM_OK;
 }
 
-static int jim_echo(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
+COMMAND_HANDLER(jim_echo)
 {
-	const char *str;
-	str = Jim_GetString(argv[1], NULL);
-	if (argc == 3 && !strcmp(str, "-n"))
+	if (CMD_ARGC == 2 && !strcmp(CMD_ARGV[0], "-n"))
 	{
-		str = Jim_GetString(argv[2], NULL);
-		LOG_USER_N("%s", str);
+		LOG_USER_N("%s", CMD_ARGV[1]);
 		return JIM_OK;
 	}
-	if (argc != 2)
+	if (CMD_ARGC != 1)
 		return JIM_ERR;
-	LOG_USER("%s", str);
+	LOG_USER("%s", CMD_ARGV[0]);
 	return JIM_OK;
 }
 
@@ -1262,6 +1259,15 @@ static const struct command_registration command_subcommand_handlers[] = {
 
 static const struct command_registration command_builtin_handlers[] = {
 	{
+		.name = "echo",
+		.handler = jim_echo,
+		.mode = COMMAND_ANY,
+		.help = "Logs a message at \"user\" priority. "
+			"Output message to stdout. "
+			"Option \"-n\" suppresses trailing newline",
+		.usage = "[-n] string",
+	},
+	{
 		.name = "add_help_text",
 		.handler = handle_help_add_command,
 		.mode = COMMAND_ANY,
@@ -1364,7 +1370,6 @@ struct command_context* command_init(const char *startup_tcl, Jim_Interp *interp
 			Jim_NewStringObj(interp, HostOs , strlen(HostOs)));
 
 	Jim_CreateCommand(interp, "ocd_find", jim_find, NULL, NULL);
-	Jim_CreateCommand(interp, "echo", jim_echo, NULL, NULL);
 	Jim_CreateCommand(interp, "capture", jim_capture, NULL, NULL);
 
 	register_commands(context, NULL, command_builtin_handlers);

doc/openocd.texi
@@ -5446,9 +5446,10 @@ file (which is normally the server's standard output).
 @xref{Running}.
 @end deffn
 
-@deffn Command echo message
+@deffn Command echo [-n] message
 Logs a message at "user" priority.
 Output @var{message} to stdout.
+Option "-n" suppresses trailing newline.
 @example
 echo "Downloading kernel -- please wait"
 @end example

commit 050fcb176071cadc7142c4d9acd3f5a9e67d3ac6

helper/command: fix echo return values
the echo command is managed through command handler and not jim_handler
to be consistent rename the handler from jim_echo to handle_echo
and update the return values

Fixes: 4747af362de0 (JIM: document "echo" command)
Change-Id: I5ae87ea802d8430b573fb83daa6b35490b5d5775
Signed-off-by: Tarek BOCHKATI <tarek.bouchkati@gmail.com>
Reviewed-on: https://review.openocd.org/c/openocd/+/6549
Tested-by: jenkins
Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>

src/helper/command.c
@@ -718,16 +718,18 @@ static int jim_find(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
 	return JIM_OK;
 }
 
-COMMAND_HANDLER(jim_echo)
+COMMAND_HANDLER(handle_echo)
 {
 	if (CMD_ARGC == 2 && !strcmp(CMD_ARGV[0], "-n")) {
 		LOG_USER_N("%s", CMD_ARGV[1]);
-		return JIM_OK;
+		return ERROR_OK;
 	}
+
 	if (CMD_ARGC != 1)
-		return JIM_ERR;
+		return ERROR_FAIL;
+
 	LOG_USER("%s", CMD_ARGV[0]);
-	return JIM_OK;
+	return ERROR_OK;
 }
 
 /* Capture progress output and return as tcl return value. If the
@@ -1219,7 +1221,7 @@ static const struct command_registration command_builtin_handlers[] = {
 	},
 	{
 		.name = "echo",
-		.handler = jim_echo,
+		.handler = handle_echo,
 		.mode = COMMAND_ANY,
 		.help = "Logs a message at \"user\" priority. "
 			"Option \"-n\" suppresses trailing newline",