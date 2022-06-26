58f987aa856a54ce34ecd402ede437a55d4181e9
Antonio Borneo <borneo.antonio@gmail.com>
2022-06-27 01:10:12 +0200

    openocd: src/rtos: replace the GPL-2.0-or-later license tag
    Replace the FSF boilerplate with the SPDX tag.
    
    The SPDX tag on files *.c is incorrect, as it should use the C99
    single line comment using '//'. But current checkpatch doesn't
    allow C99 comments, so keep using standard C comments, by now.
    
    Change-Id: If0194089baded7f58dc5d87a35d6e0aff9f43785
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: https://review.openocd.org/c/openocd/+/7070
    Tested-by: jenkins

--- src/rtos/linux.c
+++ src/rtos/linux.c
@@ -1,20 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
 /***************************************************************************
  *   Copyright (C) 2011 by STEricsson                                      *
  *   Heythem Bouhaja heythem.bouhaja@stericsson.com   : creation           *
  *   Michel JAOUEN michel.jaouen@stericsson.com : adaptation to rtos       *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  ***************************************************************************/
 
 #ifdef HAVE_CONFIG_H
