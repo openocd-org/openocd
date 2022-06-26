eb56509fa47378cdd697e043066f576cb0dc508a
Antonio Borneo <borneo.antonio@gmail.com>
2022-06-27 01:06:45 +0200

    openocd: src/jtag: replace the GPL-2.0-or-later license tag
    Replace the FSF boilerplate with the SPDX tag.
    
    The SPDX tag on files *.c is incorrect, as it should use the C99
    single line comment using '//'. But current checkpatch doesn't
    allow C99 comments, so keep using standard C comments, by now.
    
    Change-Id: Ie873d12bb0fb838d0d6252e6b9ca3c2118853e9a
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: https://review.openocd.org/c/openocd/+/7069
    Tested-by: jenkins

--- src/jtag/drivers/stlink_usb.c
+++ src/jtag/drivers/stlink_usb.c
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
 /***************************************************************************
  *   Copyright (C) 2020 by Tarek Bochkati                                  *
  *   Tarek Bochkati <tarek.bouchkati@gmail.com>                            *
@@ -13,19 +15,6 @@
  *   spen@spen-soft.co.uk                                                  *
  *                                                                         *
  *   This code is based on https://github.com/texane/stlink                *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  ***************************************************************************/
 
 #ifdef HAVE_CONFIG_H
