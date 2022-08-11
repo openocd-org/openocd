ca52cfb2b34d684d10e9c91eeb2c6a66a1448b9f
Toms Stūrmanis <toms.sturmanis@gmail.com>
2022-08-11 20:22:09 +0300

    src/flash/nor: flash driver for RSL10
    Add new flash driver for internal flash of onsemi RSL10 device.
    
    Valgrind-clean. Clang AddressSanitizer shows no errors.
    
    Signed-off-by: Toms Stūrmanis <toms.sturmanis@gmail.com>
    Change-Id: I8030542cb9805e94f56d7a69404cef5d88d6dd5a
    Reviewed-on: https://review.openocd.org/c/openocd/+/7115
    Tested-by: jenkins
    Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-by: Tomas Vanek <vanekt@fbl.cz>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -78,6 +78,7 @@ extern const struct flash_driver w600_flash;
 extern const struct flash_driver xcf_flash;
 extern const struct flash_driver xmc1xxx_flash;
 extern const struct flash_driver xmc4xxx_flash;
+extern const struct flash_driver rsl10_flash;
 
 /**
  * The list of built-in flash drivers.
@@ -153,6 +154,7 @@ static const struct flash_driver * const flash_drivers[] = {
 	&xmc1xxx_flash,
 	&xmc4xxx_flash,
 	&w600_flash,
+	&rsl10_flash,
 	NULL,
 };
 
