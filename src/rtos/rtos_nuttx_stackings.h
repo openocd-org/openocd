463df952157227866335e398102abe960ffa4131
Erhan Kurubas <erhan.kurubas@espressif.com>
2022-10-05 17:59:54 +0200

    rtos/nuttx: move stacking info to the new nuttx stackings file
    Other OSes have separate files to keep stack register offsets.
    Adding them for NuttX as well will provide a clearer way to expand.
    
    Signed-off-by: Erhan Kurubas <erhan.kurubas@espressif.com>
    Change-Id: I1428fefefa199a95431f2073c0279dd7028ad8da
    Reviewed-on: https://review.openocd.org/c/openocd/+/7250
    Tested-by: jenkins
    Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>

---
+++ src/rtos/rtos_nuttx_stackings.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef INCLUDED_RTOS_NUTTX_STACKINGS_H
+#define INCLUDED_RTOS_NUTTX_STACKINGS_H
+
+#include "rtos.h"
+
+extern const struct rtos_register_stacking nuttx_stacking_cortex_m;
+extern const struct rtos_register_stacking nuttx_stacking_cortex_m_fpu;
+
+#endif	/* INCLUDED_RTOS_NUTTX_STACKINGS_H */
