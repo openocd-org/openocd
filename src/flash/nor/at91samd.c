bd9d7b036b50d3734e308ba2812d2c63c0b3f6a5
Andrew Lalaev <andrey.lalaev@gmail.com>
2023-03-05 20:41:14 +0100

    flash/nor/at91samd: add missing SAMR34/35 part numbers
    All DIDs are taken from "SAM R34/R35 Errata Sheet" (DS80000834A).
    
    Signed-off-by: Andrew Lalaev <andrey.lalaev@gmail.com>
    Change-Id: Ie35f58e61bb02919c0676c91938c90192481d995
    Reviewed-on: https://review.openocd.org/c/openocd/+/7521
    Tested-by: jenkins
    Reviewed-by: Tomas Vanek <vanekt@fbl.cz>

--- src/flash/nor/at91samd.c
+++ src/flash/nor/at91samd.c
@@ -244,7 +244,11 @@ static const struct samd_part saml21_parts[] = {
 
     /* SAMR34/R35 parts have integrated SAML21 with a lora radio */
 	{ 0x28, "SAMR34J18", 256, 40 },
+	{ 0x29, "SAMR34J17", 128, 24 },
+	{ 0x2A, "SAMR34J16", 64, 12 },
 	{ 0x2B, "SAMR35J18", 256, 40 },
+	{ 0x2C, "SAMR35J17", 128, 24 },
+	{ 0x2D, "SAMR35J16", 64, 12 },
 };
 
 /* Known SAML22 parts. */
