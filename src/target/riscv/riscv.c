bb7852646e1b971ae88215693447235f02a60d02
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2023-07-28 22:38:23 +0300

    add diagnostics for non-implemented data watchpoints
    Change-Id: If5031c6d8cea1bfcc34bb65fd766f232498ed7ea
    Signed-off-by: Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>

--- src/target/riscv/riscv.c
+++ src/target/riscv/riscv.c
@@ -1293,6 +1293,14 @@ static void trigger_from_watchpoint(struct trigger *trigger,
 
 int riscv_add_watchpoint(struct target *target, struct watchpoint *watchpoint)
 {
+	// NOTE: typeof is needed because of upstream OpenOCD bug. This should be
+	// replaced by WATCHPOINT_IGNORE_DATA_VALUE_MASK once it is available
+	// See: https://review.openocd.org/c/openocd/+/7840
+	if (watchpoint->mask != ~(typeof(watchpoint->mask))0) {
+		LOG_TARGET_ERROR(target, "Watchpoints on data values are not implemented");
+		return ERROR_TARGET_RESOURCE_NOT_AVAILABLE;
+	}
+
 	struct trigger trigger;
 	trigger_from_watchpoint(&trigger, watchpoint);
 
