openocd-org/openocd

commit b3d4c97b9f441b1dfe36f26d64bb80974ee06ee3

jtag_vpi: fixed state transitions in "stableclocks"
In jtag_vpi_stableclocks() the TMS line needs to be set properly
based on the current JTAG TAP state (TMS=1 in TAP_RESET state vs.
TMS=0 in other cases).

Change-Id: Id49622ba83015f97b61b6a896edde4448f1fbdc6
Signed-off-by: Jan Matyas <matyas@codasip.com>
Reviewed-on: http://openocd.zylin.com/5151
Tested-by: jenkins
Reviewed-by: Franck Jullien <franck.jullien@gmail.com>
Reviewed-by: Tomas Vanek <vanekt@fbl.cz>

src/jtag/drivers/jtag_vpi.c
@@ -328,7 +328,27 @@ static int jtag_vpi_runtest(int cycles, tap_state_t state)
 
 static int jtag_vpi_stableclocks(int cycles)
 {
-	return jtag_vpi_queue_tdi(NULL, cycles, TAP_SHIFT);
+	uint8_t tms_bits[4];
+	int cycles_remain = cycles;
+	int nb_bits;
+	int retval;
+	const int CYCLES_ONE_BATCH = sizeof(tms_bits) * 8;
+
+	assert(cycles >= 0);
+
+	/* use TMS=1 in TAP RESET state, TMS=0 in all other stable states */
+	memset(&tms_bits, (tap_get_state() == TAP_RESET) ? 0xff : 0x00, sizeof(tms_bits));
+
+	/* send the TMS bits */
+	while (cycles_remain > 0) {
+		nb_bits = (cycles_remain < CYCLES_ONE_BATCH) ? cycles_remain : CYCLES_ONE_BATCH;
+		retval = jtag_vpi_tms_seq(tms_bits, nb_bits);
+		if (retval != ERROR_OK)
+			return retval;
+		cycles_remain -= nb_bits;
+	}
+
+	return ERROR_OK;
 }
 
 static int jtag_vpi_execute_queue(void)

commit 0fb31af501a8a29b097cce1d16491afe1e40066a

fixed compilation failure due to signed/unsigned comparison warning
the warning is caused by b3d4c97b in upstream OpenOCD

src/jtag/drivers/jtag_vpi.c
@@ -461,7 +461,7 @@ static int jtag_vpi_stableclocks(unsigned int num_cycles)
 	unsigned int cycles_remain = num_cycles;
 	int nb_bits;
 	int retval;
-	const int CYCLES_ONE_BATCH = sizeof(tms_bits) * 8;
+	const unsigned int CYCLES_ONE_BATCH = sizeof(tms_bits) * 8;
 
 	/* use TMS=1 in TAP RESET state, TMS=0 in all other stable states */
 	memset(&tms_bits, (tap_get_state() == TAP_RESET) ? 0xff : 0x00, sizeof(tms_bits));