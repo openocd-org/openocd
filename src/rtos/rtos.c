69736131754c1b742beb8bf3058b72ae4ffc3d32
Marc Schink <dev@zapb.de>
2024-10-23 15:02:10 +0200

    rtos: Remove 'ERROR: ' prefix in error log
    Remove the prefix since it is redundant. While at it, also
    get rid of the useless exclamation mark.
    
    Change-Id: I8707342c602cea735c5a423b37ebe40a3aafb137
    Signed-off-by: Marc Schink <dev@zapb.de>
    Reviewed-on: https://review.openocd.org/c/openocd/+/8578
    Tested-by: jenkins
    Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/rtos/rtos.c
+++ src/rtos/rtos.c
@@ -309,7 +309,7 @@ int rtos_qsymbol(struct connection *connection, char const *packet, int packet_s
 	reply_len += 2 * strlen(next_suffix);            /* hexify(..., next_suffix, ...) */
 	reply_len += 1;                                  /* Terminating NUL */
 	if (reply_len > sizeof(reply)) {
-		LOG_ERROR("ERROR: RTOS symbol '%s%s' name is too long for GDB!", next_sym->symbol_name, next_suffix);
+		LOG_ERROR("RTOS symbol '%s%s' name is too long for GDB", next_sym->symbol_name, next_suffix);
 		goto done;
 	}
 
