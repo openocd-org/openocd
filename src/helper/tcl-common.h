2968660888fcd248932e2b6561951bf26f2e3916
Antonio Borneo <borneo.antonio@gmail.com>
2025-05-09 17:54:23 +0200

    helper: add tcl-common
    Add a new helper file to start moving there the code specific to
    Jim Tcl.
    Coding with Jim Tcl require knowledge and dedicated review effort
    to properly handle its garbage collection.
    
    Having such specific helper will let us focus in a single place:
    - the knowledge of Jim Tcl garbage collection;
    - the use of Jim Tcl error codes JIM_OK and JIM_ERR;
    - the use of CamelCase symbols of Jim Tcl.
    
    Confining Jim Tcl code in a single file could also make easier the
    switch to a different Tcl library, in the remote case we need to
    address it.
    
    Change-Id: Idd89adb57e5a782f141f02d9ccdd1911f3aa9bee
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: https://review.openocd.org/c/openocd/+/9781
    Tested-by: jenkins

---
+++ src/helper/tcl-common.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef OPENOCD_HELPER_TCL_COMMON_H
+#define OPENOCD_HELPER_TCL_COMMON_H
+
+#include <jim.h>
+
+#endif /* OPENOCD_HELPER_TCL_COMMON_H */
---
+++ src/helper/tcl-libjim.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+/*
+ * This file collects all the functions to interact with Jim Tcl library.
+ *
+ * The purposes are:
+ * - to decouple the jimtcl error codes (JIM_OK, JIM_ERR, ...) from the error
+ *   codes of OpenOCD;
+ * - to decouple the internal Jim_Obj and its garbage collection;
+ * - to concentrate the Jim Tcl CamelCase symbols, now spread in OpenOCD code.
+ *
+ * The Jim Tcl CamelCase symbols used in this file should be reported in the
+ * file 'tools/scripts/camelcase.txt' to prevent errors from checkpatch.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <helper/tcl-common.h>
