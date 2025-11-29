7e508338ed0dd3ce2b5fb5b6654ff51125258812
Tomas Vanek <vanekt@fbl.cz>
2025-11-29 20:16:29 +0100

    target/riscv: add target_was_examined() checks
    If a RISC-V target does not pass initial examination
    some commands fail by assert() or log lot of hardly
    understandable errors.
    
    Check if target was examined otherwise fail early.
    Although command_print() is preferred in OpenOCD commands,
    use LOG_TARGET_ERROR() if the rest of command uses the same.
    
    Change-Id: I615e48f348a0f3bdaf71630a2fcd6fc7665115d5
    Signed-off-by: Tomas Vanek <vanekt@fbl.cz>
    Reviewed-on: https://review.openocd.org/c/openocd/+/9275
    Tested-by: jenkins
    Reviewed-by: Antonio Borneo <borneo.antonio@gmail.com>

--- src/target/riscv/riscv.c
+++ src/target/riscv/riscv.c
@@ -5007,6 +5007,11 @@ COMMAND_HANDLER(riscv_itrigger)
 	struct target *target = get_current_target(CMD_CTX);
 	const int ITRIGGER_UNIQUE_ID = -CSR_TDATA1_TYPE_ITRIGGER;
 
+	if (!target_was_examined(target)) {
+		LOG_TARGET_ERROR(target, "not examined");
+		return ERROR_TARGET_NOT_EXAMINED;
+	}
+
 	if (riscv_enumerate_triggers(target) != ERROR_OK)
 		return ERROR_FAIL;
 
@@ -5071,6 +5076,11 @@ COMMAND_HANDLER(riscv_icount)
 
 	struct target *target = get_current_target(CMD_CTX);
 	const int ICOUNT_UNIQUE_ID = -CSR_TDATA1_TYPE_ICOUNT;
+
+	if (!target_was_examined(target)) {
+		LOG_TARGET_ERROR(target, "not examined");
+		return ERROR_TARGET_NOT_EXAMINED;
+	}
 
 	if (riscv_enumerate_triggers(target) != ERROR_OK)
 		return ERROR_FAIL;
@@ -5137,6 +5147,11 @@ COMMAND_HANDLER(riscv_etrigger)
 	struct target *target = get_current_target(CMD_CTX);
 	const int ETRIGGER_UNIQUE_ID = -CSR_TDATA1_TYPE_ETRIGGER;
 
+	if (!target_was_examined(target)) {
+		LOG_TARGET_ERROR(target, "not examined");
+		return ERROR_TARGET_NOT_EXAMINED;
+	}
+
 	if (riscv_enumerate_triggers(target) != ERROR_OK)
 		return ERROR_FAIL;
 
@@ -5194,6 +5209,11 @@ COMMAND_HANDLER(riscv_etrigger)
 COMMAND_HANDLER(handle_repeat_read)
 {
 	struct target *target = get_current_target(CMD_CTX);
+	if (!target_was_examined(target)) {
+		LOG_TARGET_ERROR(target, "not examined");
+		return ERROR_TARGET_NOT_EXAMINED;
+	}
+
 	RISCV_INFO(r);
 
 	if (CMD_ARGC < 2 || CMD_ARGC > 3)
@@ -5408,6 +5428,11 @@ COMMAND_HANDLER(riscv_exec_progbuf)
 
 	struct target *target = get_current_target(CMD_CTX);
 
+	if (!target_was_examined(target)) {
+		LOG_TARGET_ERROR(target, "not examined");
+		return ERROR_TARGET_NOT_EXAMINED;
+	}
+
 	RISCV_INFO(r);
 	if (r->dtm_version != DTM_DTMCS_VERSION_1_0) {
 		LOG_TARGET_ERROR(target, "exec_progbuf: Program buffer is "
@@ -5514,6 +5539,11 @@ static COMMAND_HELPER(report_reserved_triggers, struct target *target)
 COMMAND_HANDLER(handle_reserve_trigger)
 {
 	struct target *target = get_current_target(CMD_CTX);
+	if (!target_was_examined(target)) {
+		command_print(CMD, "Error: Target not examined");
+		return ERROR_TARGET_NOT_EXAMINED;
+	}
+
 	if (CMD_ARGC == 0)
 		return CALL_COMMAND_HANDLER(report_reserved_triggers, target);
 
